43526c2b87eafa8477b6f38bdd9174e41a95cf2c
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2025-03-13 21:58:33 +0100

    MOD phaser js canvas

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,47 +1,58 @@
 import Phaser from "phaser";
 
-class Example extends Phaser.Scene
+class Broomster extends Phaser.Scene
 {
   preload ()
   {
-    this.load.setBaseURL('https://cdn.phaserfiles.com/v385');
+    // this.load.setBaseURL('https://cdn.phaserfiles.com/v385');
 
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('ship', 'ship.png');
+    this.load.image('rwr', 'screen.png');
+    this.load.image('radar', 'screen.png');
+    this.load.image('track', 'track.png');
+    this.load.image('target', 'target.png');
   }
 
   create ()
   {
-    this.add.image(400, 300, 'sky');
+    // this.add.image(400, 300, 'sky');
 
-    const particles = this.add.particles(0, 0, 'red', {
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
+    // const particles = this.add.particles(0, 0, 'red', {
+    //     speed: 100,
+    //     scale: { start: 1, end: 0 },
+    //     blendMode: 'ADD'
+    // });
 
-    const logo = this.physics.add.image(400, 100, 'logo');
+    const ship = this.physics.add.image(500, 500, 'ship');
+    this.add.image(50, 450, 'rwr');
+    this.add.text(3, 380, 'RWR', { font: '16px Courier', color: '#00ff00' });
+    this.add.image(950, 450, 'radar');
+    this.add.text(903, 380, 'RADAR', { font: '16px Courier', color: '#00ff00' });
 
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    ship.setVelocity(0, 0);
+    ship.setBounce(1, 1);
+    ship.setCollideWorldBounds(true);
 
-    particles.startFollow(logo);
+    // particles.startFollow(logo);
+  }
+
+  update ()
+  {
+    
   }
 }
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: Example,
+    width: 1000,
+    height: 500,
+    scene: Broomster,
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 0 }
+            gravity: { x: 0, y: 0 }
         }
-    }
+    },
 };
 
-const game = new Phaser.Game(config);
+new Phaser.Game(config);
--- app/src/radar/index.ts
+++ app/src/radar/index.ts
@@ -1,25 +1,27 @@
-import { Target } from "./target.ts"
-import { Vector2 } from "./vector2.ts"
-import { WorldMap } from "./map.ts"
+import Phaser from 'phaser'
+
+export class Target {
+    constructor(public position: Phaser.Math.Vector2, public direction: Phaser.Math.Vector2, public speed: number) {}
+}
 
 export class Radar {
     private range: number
     private targets: Target[]
 
-    constructor(m: WorldMap, private pos: Vector2 = new Vector2(0, 0), private pulseDir?: Vector2) {
+    constructor(m: WorldMap, private pos: Phaser.Math.Vector2 = new Phaser.Math.Vector2(), private pulseDir?: Phaser.Math.Vector2) {
         this.range = m.getRange()
         this.targets = m.getTargets()
     }
 
-    setDirection(direction: Vector2) {
+    setDirection(direction: Phaser.Math.Vector2) {
         this.pulseDir = direction
     }
 
-    setPosition(pos: Vector2) {
+    setPosition(pos: Phaser.Math.Vector2) {
         this.pos = pos
     }
 
-    transceive(d: Vector2): Target | null {
+    transceive(d: Phaser.Math.Vector2): Target | null {
         // create a target if vector equals vector to target from radar
         // add left and right tolerance
         // use sin / cos for tolerance
@@ -30,7 +32,7 @@ export class Radar {
         return null
     }
 
-    async search(socket: WebSocket, startDirection?: Vector2, endDirection?: Vector2): Promise<void> {
+    async search(socket: WebSocket, startDirection?: Phaser.Math.Vector2, endDirection?: Phaser.Math.Vector2): Promise<void> {
         const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
         if (startDirection && endDirection) {
             console.log('searchging from', startDirection, 'to', endDirection)
@@ -45,8 +47,7 @@ export class Radar {
             console.log('searching full circle')
             for (let angle = 0; angle < 360; angle++) {
                 await delay(70);
-                const radian = (angle * Math.PI) / 180
-                const direction = new Vector2(Math.cos(radian), Math.sin(radian))
+                const direction = new Phaser.Math.Vector2()
                 this.setDirection(direction)
                 socket.send(JSON.stringify({ x: direction.x, y: direction.y }));
                 const target = this.transceive(direction)
